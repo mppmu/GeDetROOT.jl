1c15ac32d89204bb78b4737fe05f50758ca9618d
Oliver Schulz <oschulz@mpp.mpg.de>
2016-03-21 00:10:36 +0100

    Using EasyPkg

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,4 @@
-using GeDetROOT
-using Base.Test
+# This file is a part of GeDetROOT, licensed under the MIT License (MIT).
 
-# write your own tests here
-@test 1 == 1
+import EasyPkg
+EasyPkg.run_all_tests()
