2144f8d78f0a7aaa5ab7153ad63c004041c3002e
Oliver Schulz <oschulz@mpp.mpg.de>
2016-06-29 13:43:40 +0200

    Added waveform output

--- bin/sis3316_to_root.jl
+++ bin/sis3316_to_root.jl
@@ -16,7 +16,7 @@ mktemp_custom(parent=tempdir(), format="tmpXXXXXX") = begin
 end
 
 
-sis3316_to_root(input_io::IO, output_tfile::TFile; evt_merge_window::AbstractFloat = 100e-9) = begin
+sis3316_to_root(input_io::IO, output_tfile::TFile; evt_merge_window::AbstractFloat = 100e-9, waveform_format = :none) = begin
     Base.time(x::Pair{Int64, SIS3316.RawChEvent}) = time(x.second)
 
     const ttoutput = TTreeOutput("data", "Data")
@@ -24,17 +24,30 @@ sis3316_to_root(input_io::IO, output_tfile::TFile; evt_merge_window::AbstractFlo
     const info_idx = ttoutput[:info_idx] = Ref{Int32}(0)
     const info_time = ttoutput[:info_time] = Ref{Float64}(0)
 
-    const raw_pp_ch = ttoutput[:raw_pp_ch] = Vector{Int32}()
-    const raw_pp_mca = ttoutput[:raw_pp_mca] = Vector{Int32}()
-    const raw_pp_trig_max = ttoutput[:raw_pp_trig_max] = Vector{Int32}()
-    const raw_pp_peak_pos = ttoutput[:raw_pp_peak_pos] = Vector{Int32}()
-    const raw_pp_peak_height = ttoutput[:raw_pp_peak_height] = Vector{Int32}()
-    const raw_pp_acc = map(i -> ttoutput[Symbol("raw_pp_acc_$i")] = Vector{Int32}(), 1:8)
+    const raw_pp_ch = ttoutput[:raw_pp_ch] = Int32[]
+    const raw_pp_mca = ttoutput[:raw_pp_mca] = Int32[]
+    const raw_pp_trig_max = ttoutput[:raw_pp_trig_max] = Int32[]
+    const raw_pp_peak_pos = ttoutput[:raw_pp_peak_pos] = Int32[]
+    const raw_pp_peak_height = ttoutput[:raw_pp_peak_height] = Int32[]
+    const raw_pp_acc = map(i -> ttoutput[Symbol("raw_pp_acc_$i")] = Int32[], 1:8)
 
-    const raw_trig_ch = ttoutput[:raw_trig_ch] = Vector{Int32}()
-    const raw_trig_trel = ttoutput[:raw_trig_trel] = Vector{Float64}()
-    const raw_trig_pileup = ttoutput[:raw_trig_pileup] = Vector{Int32}()
-    const raw_trig_overflow = ttoutput[:raw_trig_overflow] = Vector{Int32}()
+    const raw_trig_ch = ttoutput[:raw_trig_ch] = Int32[]
+    const raw_trig_trel = ttoutput[:raw_trig_trel] = Float64[]
+    const raw_trig_pileup = ttoutput[:raw_trig_pileup] = Int32[]
+    const raw_trig_overflow = ttoutput[:raw_trig_overflow] = Int32[]
+
+    const raw_wf_ch = Int32[]
+    const raw_wf_smpl_n = Int32[]
+    const raw_wf_smpl_v = Int32[]
+
+    if waveform_format == :none
+    elseif waveform_format == :integers
+        ttoutput[:raw_wf_ch] = raw_wf_ch
+        ttoutput[:raw_wf_smpl_n] = raw_wf_smpl_n
+        ttoutput[:raw_wf_smpl_v] = raw_wf_smpl_v
+    else
+        error("Unknown waveform format $waveform_format.")
+    end
 
     const ch_sized_vecs = Vector[raw_pp_ch, raw_pp_mca, raw_pp_trig_max,
         raw_pp_peak_pos, raw_pp_peak_height,
@@ -75,6 +88,10 @@ sis3316_to_root(input_io::IO, output_tfile::TFile; evt_merge_window::AbstractFlo
 
             for v in ch_sized_vecs empty!(v) end
 
+            empty!(raw_wf_ch)
+            empty!(raw_wf_smpl_n)
+            empty!(raw_wf_smpl_v)
+
             for (ch, chevt) in evtv
                 push!(raw_pp_ch, ch)
                 push!(raw_pp_mca, get(chevt.energy, energynull).maximum)
@@ -90,6 +107,14 @@ sis3316_to_root(input_io::IO, output_tfile::TFile; evt_merge_window::AbstractFlo
                 push!(raw_trig_trel, time(chevt) - starttime)
                 push!(raw_trig_pileup, chevt.pileup_flag + 2 * get(chevt.flags, flagsnull).pileup +  4 * get(chevt.flags, flagsnull).repileup)
                 push!(raw_trig_overflow, 1 * get(chevt.flags, flagsnull).overflow +  2 * get(chevt.flags, flagsnull).underflow)
+
+                if !isempty(chevt.samples)
+                    if waveform_format == :integers
+                        push!(raw_wf_ch, ch)
+                        push!(raw_wf_smpl_n, length(chevt.samples))
+                        append!(raw_wf_smpl_v, chevt.samples)
+                    end
+                end
             end
 
             push!(ttoutput)
@@ -98,7 +123,7 @@ sis3316_to_root(input_io::IO, output_tfile::TFile; evt_merge_window::AbstractFlo
 end
 
 
-sis3316_to_root(input_fname::AbstractString; evt_merge_window::AbstractFloat = 100e-9) = begin
+sis3316_to_root(input_fname::AbstractString; evt_merge_window::AbstractFloat = 100e-9, waveform_format = :none) = begin
     const fnexpr = r"(.*)\.dat(\.[^.]+)?"
     const fnbase = match(fnexpr, basename(input_fname))[1]
     const output_fname = "$(fnbase).root"
@@ -110,7 +135,7 @@ sis3316_to_root(input_fname::AbstractString; evt_merge_window::AbstractFloat = 1
         const input_io = open_decompressed(input_fname)
         const output_tfile = TFile(output_tmpname, "recreate")
 
-        sis3316_to_root(input_io, output_tfile, evt_merge_window = evt_merge_window)
+        sis3316_to_root(input_io, output_tfile, evt_merge_window = evt_merge_window, waveform_format = waveform_format)
 
         close(input_io)
         close(output_tfile)
@@ -131,6 +156,10 @@ main() = begin
             help = "time window for merging events (no merging for negative values)"
             arg_type = Float64
             default = 100e-9
+        "--waveform-format", "-w"
+            help = "waveform format (\"none\" or \"integers\")"
+            arg_type = AbstractString
+            default = "integers"
         "inputs"
             help = "input files"
             nargs = '*'
@@ -141,7 +170,11 @@ main() = begin
     for input_fname in parsed_args["inputs"]
         try
             info("Converting \"$(input_fname)\"")
-            @time sis3316_to_root(input_fname, evt_merge_window = parsed_args["merge-within"])
+            @time sis3316_to_root(
+                input_fname,
+                evt_merge_window = parsed_args["merge-within"],
+                waveform_format = symbol(parsed_args["waveform-format"])
+            )
         catch err
             print_with_color(:red, STDERR, "ERROR: $err\n")
         end
