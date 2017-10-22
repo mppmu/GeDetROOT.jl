ac017977eb1e724341e6c8cdba867ddcc43f48f4
Oliver Schulz <oschulz@mpp.mpg.de>
2017-10-22 09:54:22 +0200

    Require Julia v0.6, update CI configs, prepare for Julia v0.7

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,7 @@
 # This file is a part of GeDetROOT.jl, licensed under the MIT License (MIT).
 
-@Base.Test.testset "Package GeDetROOT" begin
+import Compat.Test
+Test.@testset "Package GeDetROOT" begin
     include.([
     ])
 end
